c213d16dc50ec337b9f5bfd2162b9e338207a647
vicky-carlo <vicky.thor.6122@gmail.com>
2024-08-04 13:04:00 +0530

    one-piece

--- src/components/videocomponent.js
+++ src/components/videocomponent.js
@@ -4,7 +4,7 @@ import ReactPlayer from "react-player"
 export default function videocomponent() {
     return(
         <div className="video">
-            <ReactPlayer url="https://youtu.be/n6Nb--zxtP0"
+            <ReactPlayer url="https://youtu.be/ExYVV3AKLOE?t=6"
 
             />
         </div>
